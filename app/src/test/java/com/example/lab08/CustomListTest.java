0910851988ccfea678a5b6e03afeb9bdd359f850
Umer-2020 <shafiumermahmood@gmail.com>
2026-03-13 16:13:02 +0500

    Failed testDeleteCity()

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -5,11 +5,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 
 public class CustomListTest {
+
+
     @Test
     public void testHasCity() {
         CustomList list = new CustomList();
         City calgary = new City("Calgary", "AB");
+
         list.addCity(calgary);
+
         assertTrue(list.hasCity(calgary));
+    }
+
+
+    @Test
+    public void testDeleteCity() {
+        CustomList list = new CustomList();
+        City edmonton = new City("Edmonton", "AB");
+
+        list.addCity(edmonton);
+        list.deleteCity(edmonton);
+
+        assertFalse(list.hasCity(edmonton));
     }
 }
